078c5ece3793ac947e12b6209266ee23169e7053
Rafsun <rafsunanik0633@gmail.com>
2026-01-05 15:10:06 -0500

    Fix syntax error

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-return "Hello from Flask! CI/CD v2 is working! 🚀"from flask import Flask
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from Flask! CI/CD v2 is working!"
+    return "Hello from Flask! CI/CD v2 is working! 🚀"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
